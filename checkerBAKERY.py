aebf5a987f473b7c9eb150cbe71b8d821e39210f
analogorange <35937401+analogorange@users.noreply.github.com>
2018-01-30 21:40:25 +0200

    Update checkerBAKERY.py

--- checkerBAKERY.py
+++ checkerBAKERY.py
@@ -1,7 +1,7 @@
 #
 # Made by analogorange for Bakery Pool
 #
-# Visit my project - Check you Garlicoin balance from anywhere: http://35.196.19.10/ or https://35.196.19.10/
+# Visit my project - Check you Garlicoin balance from anywhere: garli.top
 #
 # Donations: GemPKNS9zP4K9d7Qbc2HjTTDHYubnfMSKi
 #
